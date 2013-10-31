7d5a6fd817464ee7bdee089244136582f0601647
Steven Senkus <steven.senkus@yahoo.com>
2013-10-30 21:08:54 -0700

    hooked up

--- index.php
+++ index.php
@@ -1,16 +1,45 @@
-  <!DOCTYPE html>
-  <html>
-  <head>
-    <meta charset="utf-8">
-    <title>hello-backbonejs</title>
-    <link rel="stylesheet" type="text/css" href="css/screen.css" />
-  </head>
-  <body>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-    <script src="http://ajax.cdnjs.com/ajax/libs/json2/20110223/json2.js"></script>
-    <script src="http://ajax.cdnjs.com/ajax/libs/underscore.js/1.1.6/underscore-min.js"></script>
-    <script src="http://ajax.cdnjs.com/ajax/libs/backbone.js/0.3.3/backbone-min.js"></script>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>hello-backbonejs</title>
+        <link rel="stylesheet" type="text/css" href="css/styles.css" />
+        <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css" />
+    </head>
+    <body>
+        <div class="container">
+            <h1>User List</h1>
+            <div class="page"></div>
+            <script type="text/template" id="user-list-template">
+     <a href="#/new" class="btn btn-primary">New</a>
+    <hr />
+    <table class="table striped">
+      <thead>
+        <tr>
+          <th>First Name</th><th>Last Name</th><th>Age</th><th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <% _.each(users, function(user) { %>
+          <tr>
+            <td><%= htmlEncode(user.get('firstname')) %></td>
+            <td><%= htmlEncode(user.get('lastname')) %></td>
+            <td><%= htmlEncode(user.get('age')) %></td>
+            <td><a class="btn" href="#/edit/<%= user.id %>">Edit</a></td>
+          </tr>
+        <% }); %>
+      </tbody>
+    </table>
+            </script>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="http://ajax.cdnjs.com/ajax/libs/json2/20110223/json2.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.0/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.0/backbone-min.js"></script>
 
-    <script src="js/app.js" type="text/javascript"></script>
-  </body>
-  </html>
+
+        <script src="js/app.js" type="text/javascript"></script>
+
+    </body>
+</html>
--- js/app.js
+++ js/app.js
@@ -1,102 +1,158 @@
-(function($) {
+    function htmlEncode(value){
+      return $('<div/>').text(value).html();
+    }
+    $.fn.serializeObject = function() {
+      var o = {};
+      var a = this.serializeArray();
+      $.each(a, function() {
+          if (o[this.name] !== undefined) {
+              if (!o[this.name].push) {
+                  o[this.name] = [o[this.name]];
+              }
+              o[this.name].push(this.value || '');
+          } else {
+              o[this.name] = this.value || '';
+          }
+      });
+      return o;
+    };
 
-
-    Backbone.sync = function(method, model, success, error){
-    success();
-  }
-
-    var Item = Backbone.Model.extend({
-        defaults: {
-            part1: 'hello',
-            part2: 'world'
-        }
-    });
-    
-    var List = Backbone.Collection.extend({
-        model: Item
-    });
-
-
-    var ItemView = Backbone.View.extend({
-        tagName: 'li',
-        events: {
-            'click span': 'swap',
-            'click span.delete': 'remove'
-        },
-        initialize: function() {
-            _.bindAll(this, 'render', 'unrender', 'swap', 'remove');
-            
-            this.model.bind('change', this.render);
-            this.model.bind('remove', this.unrender);
-        },
-        render: function() {
-            $(this.el).html('<span style="color:black;">'+this.model.get('part1')+' '+this.model.get('part2')+'</span> &nbsp; &nbsp; <span class="swap" style="font-family:sans-serif; color:blue; cursor:pointer;">[swap]</span> <span class="delete" style="cursor:pointer; color:red; font-family:sans-serif;">[delete]</span>');
-            return this;
-        },
-        
-        unrender: function() {
-          $(this.el).remove();
-        },
-                
-        swap: function() {
-            var swapped = {
-                part1: this.model.get('part2'),
-                part2: this.model.get('part1')
-
-            };
-            this.model.set(swapped);
-        },
-        remove: function() {
-            this.model.destroy();
-        }
-        
+    $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
+      options.url = 'http://backbonejs-beginner.herokuapp.com' + options.url;
     });
 
-
-
-    var ListView = Backbone.View.extend({
-        el: $('body'),
-        events: {
-            'click button#add': 'addItem'
-        },
-        initialize: function() {
-            _.bindAll(this, 'render', 'addItem', 'appendItem');
-            this.collection = new List();
-            this.collection.bind('add', this.appendItem)
-            this.counter = 0;
-            this.render();
-
-        },
-        render: function() {
-            var self = this;
-            $(this.el).append('<button id="add">Add List Item</button>');
-            $(this.el).append('<ul></ul>')
-            _(this.collection.models).each(function(item){
-                self.appendItem(item);
-            }, this);
-        },
-        addItem: function() {
-            this.counter++;
-            var item = new Item();
-            item.set({
-                part2: item.get('part2') + this.counter
-                
-            });
-            this.collection.add(item);
-    
-        },
-        
-        appendItem: function(item) {
-            var itemView = new ItemView({
-                model: item
-            });
-    
-            $('ul', this.el).append(itemView.render().el);
-            
-        }
+    var Users = Backbone.Collection.extend({
+      url: '/users'
     });
 
-    var listView = new ListView();
+    var User = Backbone.Model.extend({
+      urlRoot: '/users'
+    });
 
+    var UserListView = Backbone.View.extend({
+      el: '.page',
+      render: function () {
+        var that = this;
+        var users = new Users();
+        users.fetch({
+          success: function (users) {
+            var template = _.template($('#user-list-template').html(), {users: users.models});
+            that.$el.html(template);
+          }
+        })
+      }
+    });
+var Router = Backbone.Router.extend({
+    routes: {
+        '': 'home'
+    }
 
-}(jQuery));
+});
+
+var userListView = new UserListView();
+
+var router = new Router();
+router.on('route:home', function() {
+    console.log('route:home');
+    userListView.render();
+
+});
+
+Backbone.history.start();
+
+/*
+ Backbone.sync = function(method, model, success, error) {
+ success();
+ };
+ 
+ var Item = Backbone.Model.extend({
+ defaults: {
+ part1: 'hello',
+ part2: 'world'
+ }
+ });
+ 
+ var List = Backbone.Collection.extend({
+ model: Item
+ });
+ 
+ 
+ var ItemView = Backbone.View.extend({
+ tagName: 'li',
+ events: {
+ 'click span': 'swap',
+ 'click span.delete': 'remove'
+ },
+ initialize: function() {
+ _.bindAll(this, 'render', 'unrender', 'swap', 'remove');
+ 
+ this.model.bind('change', this.render);
+ this.model.bind('remove', this.unrender);
+ },
+ render: function() {
+ $(this.el).html('<span style="color:black;">' + this.model.get('part1') + ' ' + this.model.get('part2') + '</span> &nbsp; &nbsp; <span class="swap" style="font-family:sans-serif; color:blue; cursor:pointer;">[swap]</span> <span class="delete" style="cursor:pointer; color:red; font-family:sans-serif;">[delete]</span>');
+ return this;
+ },
+ unrender: function() {
+ $(this.el).remove();
+ },
+ swap: function() {
+ var swapped = {
+ part1: this.model.get('part2'),
+ part2: this.model.get('part1')
+ 
+ };
+ this.model.set(swapped);
+ },
+ remove: function() {
+ this.model.destroy();
+ }
+ 
+ });
+ 
+ 
+ 
+ var ListView = Backbone.View.extend({
+ el: $('body'),
+ events: {
+ 'click button#add': 'addItem'
+ },
+ initialize: function() {
+ _.bindAll(this, 'render', 'addItem', 'appendItem');
+ this.collection = new List();
+ this.collection.bind('add', this.appendItem)
+ this.counter = 0;
+ this.render();
+ 
+ },
+ render: function() {
+ var self = this;
+ $(this.el).append('<button id="add">Add List Item</button>');
+ $(this.el).append('<ul></ul>')
+ _(this.collection.models).each(function(item) {
+ self.appendItem(item);
+ }, this);
+ },
+ addItem: function() {
+ this.counter++;
+ var item = new Item();
+ item.set({
+ part2: item.get('part2') + this.counter
+ 
+ });
+ this.collection.add(item);
+ 
+ },
+ appendItem: function(item) {
+ var itemView = new ItemView({
+ model: item
+ });
+ 
+ $('ul', this.el).append(itemView.render().el);
+ 
+ }
+ });
+ 
+ var listView = new ListView();
+ 
+ */
