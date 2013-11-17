e60c529518e16374b199f7e78bdba2fdc9b55612
Steven Senkus <steven.senkus@yahoo.com>
2013-11-17 14:41:10 -0800

    the beginnings of a cool carousel/slider

--- js/app.js
+++ js/app.js
@@ -1,18 +1,44 @@
-var SearchView = Backbone.View.extend({
-    el: '#main',
-    initialize: function() {
-        this.render();
-    },
-    render: function() {
-        // Compile the template using underscore
-        var template = _.template($("#search_template").html(), {search_label: 'dfgdf'});
-        // Load the compiled HTML into the Backbone "el"
-        this.$el.html(template);
-    }
-});
+$(document).ready(function() {
 
-var search_view = new SearchView({el: $("#search_container")});
-var arrivals = Backbone.Collection.extend({
-      arrival: model.arrival,
-      localStorage: new Store("backbone-todo")
+    var Slides = Backbone.Model.extend({
+        defaults: {
+            description: 'this is a test description'
+            
+        },
+        initialize: function() {
+            
+            
+        }
+        
     });
+
+    var CarouselSlides = Backbone.Collection.extend({
+        model: Slides
+    });
+
+
+    var CarouselView = Backbone.View.extend({
+        initialize: function() {
+            this.render();
+        },
+        render: function() {
+            var template = _.template($("#slide0-content").html(), {title: 'Slide 0 Title'});
+            // Load the compiled HTML into the Backbone "el"
+                        $('#carousel0').carousel();
+            this.$el.html(template);
+
+        },
+        events: {
+            'click #btnInfo': 'btnGetInfo'
+
+        },
+        btnGetInfo: function() {
+            $('#myModal').modal({
+                remote: 'modals/mountain.html'
+            });
+        }
+    });
+    
+    var carousel = new CarouselView({el: $('#carousel-inner')});
+    
+});
