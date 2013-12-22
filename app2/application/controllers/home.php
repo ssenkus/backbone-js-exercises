1aa55a022e81d1c5da583640899f23c438cf52b9
Steven Senkus <steven.senkus@yahoo.com>
2013-12-21 23:50:43 -0800

    experiments

--- app2/application/controllers/home.php
+++ app2/application/controllers/home.php
@@ -1,24 +1,7 @@
 <?php
-require(APPPATH.'/libraries/REST_Controller.php');
-class home extends REST_Controller {
 
-    public function index_get() {
-        $out = array(
-            'success' => 'true', 
-            'data' => array(
-                'a' => 'asd',
-                'b' => 'zxc',
-                'c' => 'qwe'
-        ));
-        //var_dump($out);
-        $this->response($out);
-        // Display all books
-    }
-
-    public function index_post() {
-        // Create a new book
-    }
-
-}
-
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 ?>
