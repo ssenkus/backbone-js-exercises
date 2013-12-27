0ea0ef404bb1b03d05a0e578c33779f9a790f3c1
Steven Senkus <steven.senkus@yahoo.com>
2013-12-26 22:07:50 -0800

    fixin' up calc form

--- exp/app/views/CalculationForm.js
+++ exp/app/views/CalculationForm.js
@@ -1,25 +1,27 @@
 var CalculationForm = Backbone.View.extend({
+    events: {
+        'submit form': 'submitForm' // not working
+    },
+    submitForm: function(e) {
+        
+        e.preventDefault();
+        alert('this form shoulda submitted, eh?!')
+        
+    },
     initialize: function(options) {
         this.subTotal = options.subTotal || 0;
         console.log('calform options', options)
-        this.collection = new Formulas([
-            new Formula({}),
-            new Formula({}),
-            new Formula({})
-        ]);
+        this.collection = app.chosenTable.collection;
         this.render();
     },
     render: function() {
-
         var view = this;
-        $.when(
-                $('#calculationForm').html(_.template($('#calculation-form').html(), {formulas: view.collection.models, subTotal: this.subTotal})))
-                .then(function() {
+        $.when($('#calculationForm').html(_.template($('#calculation-form').html(), {formulas: view.collection.models, subTotal: this.subTotal})))
+            .then(function() {
             view.setElement($("#resultTable"));
         });
         return this;
     }
-
 
 });
 
