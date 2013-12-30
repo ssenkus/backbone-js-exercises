b5c6327cb913c9b82f6e1f1f2903590c7661cade
Steven Senkus <steven.senkus@yahoo.com>
2013-12-29 17:01:55 -0800

    update the FormulaModel defaults

--- exp/app/models/FormulaTotal.js
+++ exp/app/models/FormulaTotal.js
@@ -1,7 +1,8 @@
 var FormulaTotal = Backbone.Model.extend({
     defaults: {
         subTotal: -1,
-        shipping: 'none selected'
+        shipping: 'none selected',
+        discount: 'no discount selected'
     }
     
 });
