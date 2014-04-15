87707387a27827a976a057f90395c03e636b6c71
Steven Senkus <steven.senkus@yahoo.com>
2014-04-15 11:40:12 -0700

    basic JS + PHP/CI functionality

--- app2/application/controllers/arrivals.php
+++ app2/application/controllers/arrivals.php
@@ -7,13 +7,13 @@ class arrivals extends REST_Controller {
     public function index_get() {
 
 // Initializing curl
-        $ch = curl_init('http://developer.trimet.org/ws/V1/arrivals/locIDs/2580/appID/5E48DCD7031297B7CBF2F37FD?json=true');
-
+        //       $ch = curl_init('http://developer.trimet.org/ws/V1/arrivals/locIDs/2580/appID/5E48DCD7031297B7CBF2F37FD?json=true');
+        $ch = curl_init('http://developer.trimet.org/ws/V1/arrivals/?appID=5E48DCD7031297B7CBF2F37FD&locIDs=2580&json=true');
 // Configuring curl options
         $options = array(
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_HTTPHEADER => array('Content-type: application/json'),
-            //CURLOPT_POSTFIELDS => $json_string
+                //CURLOPT_POSTFIELDS => $json_string
         );
 
 // Setting curl options
@@ -21,12 +21,11 @@ class arrivals extends REST_Controller {
 
 // Getting results
         $result = curl_exec($ch); // Getting jSON result string        
-
-        $this->response($result);
-        // Display all books
+        $this->response(($result));
     }
+
     public function index_post() {
-        // Create a new book
+        
     }
 
 }
